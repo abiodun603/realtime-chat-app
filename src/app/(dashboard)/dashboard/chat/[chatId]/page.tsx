58b76f4d2afb1d51e424f1dc2c7a6a5bda9e894c
abiodun603 <abioduncsc@gmail.com>
2024-02-13 22:57:12 +0100

    feat: chat header

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { messageArrayValidator } from '@/lib/validation/messages'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 interface PageProps {
@@ -63,7 +64,29 @@ const page = async ({params}: PageProps) => {
 
 
   return (
-    <div>page</div>
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6remo)]'>
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+              <Image
+                fill
+                referrerPolicy='no-referrer'
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className='rounded-full'
+              />
+            </div>
+          </div>
+          <div className="flex flex-col leading-tight">
+            <div className="text-xl flex items-centr">
+              <span className='text-gray-700 mr-3 font-semibold'>{chatPartner.name}</span>
+            </div>
+            <span className="text-sm text-gray-600">{chatPartner.email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
